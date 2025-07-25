a4b4af92d8d16e5c4fe1238cfeda8130abd25468
cofheim <plastininbiz@yandex.ru>
2025-07-25 18:35:07 +0300

    фича: добавлено автоматическое применение миграций при старте

--- ProfitTest.API/Program.cs
+++ ProfitTest.API/Program.cs
@@ -50,6 +50,12 @@ services.AddDbContext<ProfitTestDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ProfitTestDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Middleware
 if (app.Environment.IsDevelopment())
 {
